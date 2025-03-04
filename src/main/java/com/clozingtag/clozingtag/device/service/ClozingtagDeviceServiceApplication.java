91b9cb644e3383ac3fcdc8d0375cc1494fd8e40d
Saint Deemene <deemene.saint@loblaw.ca>
2025-03-04 14:46:52 +0000

    Project setup
    Setting up Device Service

---
+++ src/main/java/com/clozingtag/clozingtag/device/service/ClozingtagDeviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clozingtag.clozingtag.device.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClozingtagDeviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClozingtagDeviceServiceApplication.class, args);
+	}
+
+}
